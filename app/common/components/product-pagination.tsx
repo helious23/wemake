30ea993d9c2a184e244176c1eb71ca844d9e5ced
Max16 <max16@naver.com>
2025-05-31 16:30:53 +0900

    #3.17 Pagination with Link

--- app/common/components/product-pagination.tsx
+++ app/common/components/product-pagination.tsx
@@ -1,3 +1,14 @@
+import { useSearchParams } from "react-router";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "./ui/pagination";
+
 type ProductPaginationProps = {
   totalPages: number;
 };
@@ -5,5 +16,54 @@ type ProductPaginationProps = {
 export default function ProductPagination({
   totalPages,
 }: ProductPaginationProps) {
-  return <div>ProductPagination</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const page = Number(searchParams.get("page")) ?? "1";
+  if (isNaN(page) || page < 1 || page > totalPages) {
+    return null;
+  }
+
+  return (
+    <div>
+      <Pagination>
+        <PaginationContent>
+          {page === 1 ? null : (
+            <>
+              <PaginationItem>
+                <PaginationPrevious to={`?page=${page - 1}`} />
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink to={`?page=${page - 1}`}>
+                  {page - 1}
+                </PaginationLink>
+              </PaginationItem>
+            </>
+          )}
+          <PaginationItem>
+            <PaginationLink to={`?page=${page}`} isActive>
+              {page}
+            </PaginationLink>
+          </PaginationItem>
+          {page === totalPages ? null : (
+            <>
+              <PaginationItem>
+                <PaginationLink to={`?page=${page + 1}`}>
+                  {page + 1}
+                </PaginationLink>
+              </PaginationItem>
+              {page + 1 === totalPages ? null : (
+                <>
+                  <PaginationItem>
+                    <PaginationEllipsis />
+                  </PaginationItem>
+                </>
+              )}
+              <PaginationItem>
+                <PaginationNext to={`?page=${page + 1}`} />
+              </PaginationItem>
+            </>
+          )}
+        </PaginationContent>
+      </Pagination>
+    </div>
+  );
 }
